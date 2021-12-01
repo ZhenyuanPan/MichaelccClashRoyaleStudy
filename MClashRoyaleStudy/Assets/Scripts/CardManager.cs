2f85a2049a043c18a6976e121cc0ba956136810c
Michael <1656922908@qq.com>
2021-12-01 17:45:29 +0800

    完成了出牌功能

--- MClashRoyaleStudy/Assets/Scripts/CardManager.cs
+++ MClashRoyaleStudy/Assets/Scripts/CardManager.cs
@@ -5,41 +5,59 @@ using DG.Tweening;
 
 public class CardManager : MonoBehaviour
 {
+    public static CardManager instance;
     public GameObject cardPrefab;
-    public Transform[] cards = new Transform[4];
+    //我有四张出牌占位
+    public Transform[] cardsPlaceholder = new Transform[4];
     public Transform startPos, endPos;
     private Transform previewCard;
     public Transform canvas;
+    private void Awake()
+    {
+        instance = this;
+    }
     private void Start()
     {
         canvas.gameObject.SetActive(true);
         StartCoroutine(CreateCardInPreviewArea(.1f));
-        print(cards.Length);
-        for (int i = 0; i < cards.Length; i++)
+        print(cardsPlaceholder.Length);
+        for (int i = 0; i < cardsPlaceholder.Length; i++)
         {
             StartCoroutine(PromoteFromDeck(i,.4f+i));
             StartCoroutine(CreateCardInPreviewArea(.8f+i));
         }
 
     }
-    IEnumerator CreateCardInPreviewArea(float delay) 
+    public IEnumerator CreateCardInPreviewArea(float delay) 
     {
         yield return new WaitForSeconds(delay);
         print($"addToDeck");
+        //range函数左闭右开, 值最大为list.count-1
         int iCard = Random.Range(0,MyCardModel.instance.list.Count);
         MyCard card = MyCardModel.instance.list[iCard];
         GameObject cardPrefab = Resources.Load<GameObject>(card.cardPrefab);
-
         previewCard = Instantiate(cardPrefab, canvas).transform;
         previewCard.position = startPos.position;
         previewCard.localScale = Vector3.one * .7f;
         previewCard.DOMove(endPos.position,.2f);
+        
+        //给卡牌view 的数据赋值, 在previewArea(预览区中) 不决定是第几张卡牌, 在出牌区决定是第几张牌,是否准许点击事件.
+        previewCard.GetComponent<MyCardView>().data = card;
+
     }
-    IEnumerator PromoteFromDeck(int i,float delay) 
+    public IEnumerator PromoteFromDeck(int i,float delay) 
     {
         yield return new WaitForSeconds(delay);
         print($"PromoteFromDeck");
         previewCard.localScale = Vector3.one;
-        previewCard.transform.DOMove(cards[i].position,.2f+.05f*i);
+        previewCard.transform.DOMove(
+            cardsPlaceholder[i].position,.2f+.05f*i)
+            .OnComplete(
+            ()=> {
+                //完成卡牌到出牌区动画后给卡牌view层赋值, 并准许其移动
+                var mycardview = previewCard.GetComponent<MyCardView>();
+                mycardview.isInteraction = true;
+                mycardview.index = i;
+            });
     }
 }
---
+++ MClashRoyaleStudy/Assets/Scripts/MyPlaceableView.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyPlaceableView : MonoBehaviour
+{
+   public MyPlaceable data; // 游戏单位数据
+
+}
